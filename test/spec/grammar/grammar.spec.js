4cf3523f57603fa5b0cc0a02843f207d0d2064b7
Mathieu Abou-Aichi <mathieu.aa@protonmail.com>
2019-12-08 16:29:04 +0100

    Fixed test titles and expectations

--- test/spec/grammar/grammar.spec.js
+++ test/spec/grammar/grammar.spec.js
@@ -48,7 +48,7 @@ MAX = 43`);
       expect(constants).to.deep.equal({ MIN: 42, MAX: 43 });
     });
   });
-  context('when parsing applications', () => {
+  context('when parsing an application', () => {
     context('with no custom configuration', () => {
       let application;
 
@@ -177,10 +177,10 @@ application {
             application = content.applications[0];
           });
 
-          it('should parse the list', () => {
+          it('should parse the entity list', () => {
             expect(application.entities.entityList).to.deep.equal(['*']);
           });
-          it('should parse the list', () => {
+          it('should parse the excluded list', () => {
             expect(application.entities.excluded).to.deep.equal(['A']);
           });
         });
@@ -198,10 +198,10 @@ application {
             application = content.applications[0];
           });
 
-          it('should parse the list', () => {
+          it('should parse the entity list', () => {
             expect(application.entities.entityList).to.deep.equal(['*']);
           });
-          it('should parse the list', () => {
+          it('should parse the excluded list', () => {
             expect(application.entities.excluded).to.deep.equal(['A']);
           });
         });
@@ -386,7 +386,7 @@ application {
           expect(parsedEntity).to.deep.equal({
             annotations: [],
             body: [],
-            javadoc: '\n * Big \n * comment.\n ',
+            javadoc: '\n * Big\n * comment.\n ',
             name: 'A',
             tableName: 'A'
           });
